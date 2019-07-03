581327e31ebb1f8a08467b050826c55c795fd913
MartaSadova <sadovamarta3@ukr.net>
2019-07-04 00:48:34 +0300

    homework5

--- js/homework4.js
+++ js/homework4.js
@@ -1,24 +1,27 @@
 //task1
 //додавати елементи в масив через метод prompt() поки користувавч ненатисне cancel
 var arr = [];
-for (i = 0; true; i++){
-  var num = prompt('Input your digit', '0');
-  if (num == null){
-    break;
-  }
-  else {
-    arr.push(num);
-  }
+for (i = 0; true; i++) {
+    var num = prompt('Input your digit', '0');
+    if (num == null) {
+        break;
+    } else {
+        arr.push(num);
+    }
 }
 console.log(arr);
 
 
 //task2
 //знайти суму елементів двовимірного масиву
-var numbers = [ [12, 11, 16, 17], [2, 6, 5, 8, 8], [254, 122, 110] ];
+var numbers = [
+    [12, 11, 16, 17],
+    [2, 6, 5, 8, 8],
+    [254, 122, 110]
+];
 var sum = 0;
-for (num of numbers){
-  sum += num.length;
+for (num of numbers) {
+    sum += num.length;
 }
 console.log(sum);
 
@@ -38,7 +41,7 @@ for (var word of strArr) {
 //дано масив [1, 9, 22, 7, 6] додати число 8 після 22
 var arr = [1, 9, 22, 7, 6];
 console.log(arr);
-arr.splice(3, 0, 8); 
+arr.splice(3, 0, 8);
 console.log(arr);
 
 
@@ -47,17 +50,14 @@ console.log(arr);
 //випадковими числами від 0 до 100, де number довжина масиву
 var min = 0;
 var max = 100;
-var mas = new Array();
-var n = 2, m = 3;
 
-for(i = 0; i < n; i++)
-mas[i] = new Array (m);
-for(i = 0; i < n; i++)
-for(j = 0; j < m; j++)
-mas[i][j]= Math.floor ( Math.random() * ( max-min + 1 ) ) + min; 
-
-console.log( mas );
-
+function randomNumber(number) {
+    var mas = new Array(number); //створює масив без елементів, але довжиною (number)
+    for (i = 0; i < mas.length; i++)
+        mas[i] = Math.floor(Math.random() * (max - min + 1)) + min;
+    return mas;
+}
+console.log(randomNumber(5));
 
 //task6
 //перевірити чи в масивові є число, яке вводиться через prompt()
@@ -72,28 +72,16 @@ console.log(mas2.indexOf(prompt()));
 var str = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia beatae voluptate quos quod, consectetur saepe nihil consequatur ipsa animi sint ipsam, enim quasi eveniet dolorem cumque libero modi. Nisi, voluptatum.';
 var arr = str.split(' ');
 arr.forEach(function(item, i, arr) {
-   arr.sort(function(a, b){
-     return b.length - a.length;
-   });
-   console.log(arr [0] + ' - найдовше слово в строці lorem');
+    arr.sort(function(a, b) {
+        return b.length - a.length;
+    });
+    console.log(arr[0] + ' - найдовше слово в строці lorem');
 });
 
 //task8
 //за допомогою методу map створити масив квадратів масиву arr = [8, 6, 12, 10];
 var numbers = [8, 6, 12, 10];
 var numbersInTheSquare = numbers.map(function(number) {
-  return number ** 2;
+    return number ** 2;
 });
 console.log(numbersInTheSquare);
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ js/homework5.js
@@ -0,0 +1,50 @@
+// task1
+// Знайти в об’єкті city, місто з найбільшою кількістю населення
+var city = {
+  "Zarephath": 728375,
+  "Reinerton": 864402,
+  "Spelter": 340533,
+  "Henrietta": 932557,
+  "Dyckesville": 421758,
+  "Yettem": 250492,
+  "Gracey": 551885,
+  "Floris": 216435,
+  "Davenport": 290139,
+  "Tioga": 653031
+}
+
+var sortableCity = [];
+for (var place in city) {
+  sortableCity.push([place, city[place]]);
+}
+sortableCity.sort(function(a, b) {
+  return b[1] - a[1];
+});
+console.log(sortableCity [0] [0]);
+
+// the second way
+citySorted = Object.keys(city).sort(function(a, b) {
+  return city[b] - city[a]
+});
+console.log(citySorted[0]);
+
+
+// task2
+class Worker {
+  constructor(name, surname, rate, days){
+    this.name = name;
+    this.surname = surname;
+    this.rate = rate;
+    this.days = days;
+  }
+  getSalary() {
+    return this.rate * this.days;
+  }
+}
+
+var worker = new Worker ('Ivan', 'Ivanov', 10, 31);
+console.log (worker.name);
+console.log (worker.surname); 
+console.log (worker.rate); 
+console.log (worker.days); 
+console.log (worker.getSalary ());
